0c7bdbeba20ce599ee33d40428ebcafd7a89a6d9
BestSucces <uniquegreatness1@gmail.com>
2023-02-20 16:21:57 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desription - This progrmn is to print string of words
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\n"
+			- Dora Korpar, 2015 - 10 - 19\n",59)
+	return (0);
+
+
+}
