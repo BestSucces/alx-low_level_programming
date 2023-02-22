8b5769a6a1fa669874e5e029ec0d5f081431786b
BestSucces <uniquegreatness1@gmail.com>
2023-02-22 14:13:55 -0800

    lower case identifier

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - print lowercase alphabets
+ *
+ * @c: parameter to be printed
+ * Return: 1 if it is lowercase
+ * and otherwise 0
+ */
+int _islower(int c)
+{
+
+	if (c >= 97 &&c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
