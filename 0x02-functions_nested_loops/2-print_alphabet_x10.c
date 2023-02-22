c553239539c845bd5b7b693572bc5d634ca2728c
BestSucces <uniquegreatness1@gmail.com>
2023-02-22 13:31:47 -0800

    ptint ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - this function is to print lowercase ten times
+ * return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 1 ; i <= 0 ; i++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+
+}
