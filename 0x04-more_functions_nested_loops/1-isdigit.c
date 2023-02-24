f79a0148c9ec2f13ec0f24c3b81a68e101879bfa
BestSucces <uniquegreatness1@gmail.com>
2023-02-24 07:41:48 -0800

    search digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,17 +9,15 @@
  */
 int _isdigit(int c)
 {
-	int i;
-
-	for (i = 0 ; i <= 9 ; i++)
 	if  (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
 	else
-	{	return (0);
+	{
+		return (0);
 	}
-	return (0);	
+	return (0);
 
 }
 
