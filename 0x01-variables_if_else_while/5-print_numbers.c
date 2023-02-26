0f402a7fb6eaf1cfef5e0731208c09d56e687618
BestSucces <uniquegreatness1@gmail.com>
2023-02-25 18:51:18 -0800

    single base 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point of the function
+ * Return: always o;
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
