fa400c3af681dd0981ab8e9963bdce71743666c6
BestSucces <uniquegreatness1@gmail.com>
2023-02-25 17:48:48 -0800

    alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 /**
+ * main - entry point of the function
  *
- *
- *
+ * Return: always 0 (succes)
  */
+int main(void)
+{
+	char n;
 
+	for (n = 'a' ; n <= 'z' ; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+
+}
