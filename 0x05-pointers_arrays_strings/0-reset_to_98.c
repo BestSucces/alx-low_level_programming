8d158a4095947f32cef72fa5c540d3cc82aad329
BestSucces <uniquegreatness1@gmail.com>
2023-02-27 20:21:29 -0800

    print every other charater of string

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * reset__to_98 - 
+ *
+ *
+ *
+ */
+void reset_to_98(int *n);
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * put2 - function prints every other character of string
+ * @str: function parameter
+ * Return: always 0
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
