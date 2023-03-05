fd778be392405a2e350a966805af32f830ec8dd9
BestSucces <uniquegreatness1@gmail.com>
2023-03-05 06:37:44 -0800

    print size of c type on computer used

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Desription - This progrmn is to print string of words
  *
- * Return: Always 0 (sucess)
+ * Return: Always 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\n"
-			- Dora Korpar, 2015 - 10 - 19\n",59)
-	return (0);
+	write(1, "and that piece of art is useful\"
+			- Dora Korpar, 2015 - 10 - 19\n", 59)
+	return (1);
 
 
 }
